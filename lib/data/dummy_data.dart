edb00694304124bc7e17db80d68dc58694efe879
jonak1064 <124136656+jonak1064@users.noreply.github.com>
2024-04-14 19:17:16 +0600

    “data”

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -12,4 +12,161 @@ const dummyTeams = [
   Team(
       id: 't4', name: 'England', imageUrl: 'assets/images/team-4.png', rank: 4),
   Team(id: 't5', name: 'Brazil', imageUrl: 'assets/images/team-5.png', rank: 5),
+  Team(
+      id: 't6',
+      name: 'Portugal',
+      imageUrl: 'assets/images/team-6.png',
+      rank: 6),
+  Team(
+      id: 't7',
+      name: 'Netherland',
+      imageUrl: 'assets/images/team-7.png',
+      rank: 7),
+  Team(id: 't8', name: 'Spain', imageUrl: 'assets/images/team-8.png', rank: 8),
+  Team(id: 't9', name: 'Italy', imageUrl: 'assets/images/team-9.png', rank: 9),
+  Team(
+      id: 't10',
+      name: 'Crotia',
+      imageUrl: 'assets/images/team-10.png',
+      rank: 10),
+  Team(id: 't11', name: 'USA', imageUrl: 'assets/images/team-11.png', rank: 11),
+  Team(
+      id: 't12',
+      name: 'Colombia',
+      imageUrl: 'assets/images/team-12.png',
+      rank: 12),
+  Team(
+      id: 't13',
+      name: 'Morocco',
+      imageUrl: 'assets/images/team-13.png',
+      rank: 13),
+  Team(
+      id: 't14',
+      name: 'Mexico',
+      imageUrl: 'assets/images/team-14.png',
+      rank: 14),
+  Team(
+      id: 't15',
+      name: 'Uruguay',
+      imageUrl: 'assets/images/team-15.png',
+      rank: 15),
+  Team(
+      id: 't16',
+      name: 'Germany',
+      imageUrl: 'assets/images/team-16.png',
+      rank: 16),
+  Team(
+      id: 't17',
+      name: 'Senegal',
+      imageUrl: 'assets/images/team-17.png',
+      rank: 17),
+  Team(
+      id: 't18',
+      name: 'Japan',
+      imageUrl: 'assets/images/team-18.png',
+      rank: 18),
+  Team(
+      id: 't19',
+      name: 'Switzerland',
+      imageUrl: 'assets/images/team-19.png',
+      rank: 19),
+  Team(
+      id: 't20', name: 'Iran', imageUrl: 'assets/images/team-20.png', rank: 20),
+  Team(
+      id: 't21',
+      name: 'Denmark',
+      imageUrl: 'assets/images/team-21.png',
+      rank: 21),
+  Team(
+      id: 't22',
+      name: 'Ukraine',
+      imageUrl: 'assets/images/team-22.png',
+      rank: 22),
+  Team(
+      id: 't23',
+      name: 'Korea',
+      imageUrl: 'assets/images/team-23.png',
+      rank: 23),
+  Team(
+      id: 't24',
+      name: 'Australia',
+      imageUrl: 'assets/images/team-24.png',
+      rank: 24),
+  Team(
+      id: 't25',
+      name: 'Austria',
+      imageUrl: 'assets/images/team-25.png',
+      rank: 25),
+  Team(
+      id: 't26',
+      name: 'Hungary',
+      imageUrl: 'assets/images/team-26.png',
+      rank: 26),
+  Team(
+      id: 't27',
+      name: 'Sweden',
+      imageUrl: 'assets/images/team-27.png',
+      rank: 27),
+  Team(
+      id: 't28',
+      name: 'Poland',
+      imageUrl: 'assets/images/team-28.png',
+      rank: 28),
+  Team(
+      id: 't29',
+      name: 'Wales',
+      imageUrl: 'assets/images/team-29.png',
+      rank: 29),
+  Team(
+      id: 't30',
+      name: 'Nigeria',
+      imageUrl: 'assets/images/team-30.png',
+      rank: 30),
+  Team(
+      id: 't31',
+      name: 'Ecuador',
+      imageUrl: 'assets/images/team-31.png',
+      rank: 31),
+  Team(
+      id: 't32', name: 'Peru', imageUrl: 'assets/images/team-32.png', rank: 32),
+  Team(
+      id: 't33',
+      name: 'Serbia',
+      imageUrl: 'assets/images/team-33.png',
+      rank: 33),
+  Team(
+      id: 't34',
+      name: 'Qatar',
+      imageUrl: 'assets/images/team-34.png',
+      rank: 34),
+  Team(
+      id: 't35',
+      name: 'Russia',
+      imageUrl: 'assets/images/team-35.png',
+      rank: 35),
+  Team(
+      id: 't36',
+      name: 'Czech Republic',
+      imageUrl: 'assets/images/team-36.png',
+      rank: 36),
+  Team(
+      id: 't37',
+      name: 'Egypt',
+      imageUrl: 'assets/images/team-37.png',
+      rank: 37),
+  Team(
+      id: 't38',
+      name: 'Ivory Coast',
+      imageUrl: 'assets/images/team-38.png',
+      rank: 38),
+  Team(
+      id: 't39',
+      name: 'Scotland',
+      imageUrl: 'assets/images/team-39.png',
+      rank: 39),
+  Team(
+      id: 't40',
+      name: 'Turkey',
+      imageUrl: 'assets/images/team-40.png',
+      rank: 40),
 ];
--- lib/widgets/first_column.dart
+++ lib/widgets/first_column.dart
@@ -12,6 +12,7 @@ class FirstColumn extends StatelessWidget {
               'assets/images/teams-1.png',
               height: 50,
             ),
+            const SizedBox(width: 5),
             ElevatedButton(
               onPressed: () {},
               child: const Text('Select'),
@@ -27,6 +28,7 @@ class FirstColumn extends StatelessWidget {
               'assets/images/teams-2.png',
               height: 50,
             ),
+            const SizedBox(width: 5),
             ElevatedButton(
               onPressed: () {},
               child: const Text('Select'),
@@ -42,6 +44,7 @@ class FirstColumn extends StatelessWidget {
               'assets/images/teams-3.png',
               height: 50,
             ),
+            const SizedBox(width: 5),
             ElevatedButton(
               onPressed: () {},
               child: const Text('Select'),
@@ -57,6 +60,7 @@ class FirstColumn extends StatelessWidget {
               'assets/images/teams-4.png',
               height: 50,
             ),
+            const SizedBox(width: 5),
             ElevatedButton(
               onPressed: () {},
               child: const Text('Select'),
@@ -72,6 +76,87 @@ class FirstColumn extends StatelessWidget {
               'assets/images/teams-5.png',
               height: 50,
             ),
+            const SizedBox(width: 5),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Select'),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Row(
+          children: [
+            Image.asset(
+              'assets/images/teams-6.png',
+              height: 50,
+            ),
+            const SizedBox(width: 5),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Select'),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Row(
+          children: [
+            Image.asset(
+              'assets/images/teams-7.png',
+              height: 50,
+            ),
+            const SizedBox(width: 5),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Select'),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Row(
+          children: [
+            Image.asset(
+              'assets/images/teams-8.png',
+              height: 50,
+            ),
+            const SizedBox(width: 5),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Select'),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Row(
+          children: [
+            Image.asset(
+              'assets/images/teams-9.png',
+              height: 50,
+            ),
+            const SizedBox(width: 5),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Select'),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Row(
+          children: [
+            Image.asset(
+              'assets/images/teams-10.png',
+              height: 50,
+            ),
+            const SizedBox(width: 5),
             ElevatedButton(
               onPressed: () {},
               child: const Text('Select'),
